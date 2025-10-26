0979c914715be283630ff5e139c01ba97933edf9
Rithiga <rithiga2026@gmail.com>
2025-10-26 14:43:00 +0530

    Added user routes and controller logic

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,19 +2,50 @@ import express = require("express");
 import asyncHandler from "express-async-handler";
 import { Response,Request,NextFunction } from "express";
 
+import{ createUserService, deleteUserService, getUserService, updateUserService} from "../services/userService";
+import { updateTransactionService } from "../services/transactionService";
 const createUser = asyncHandler(async(req:Request,res:Response)=>{
-   
+   const user = await createUserService(
+    req.body.email,
+    req.body.fullName,
+    req.body.password
+   )
+   res.status(201).json(user);
 });
 
 const getUser = asyncHandler(async(req:Request,res:Response)=>{
+    const user = await getUserService(
+        Number(req.body.params)
+    )
+    if(!user){
+        res.status(404)
+        throw new Error("user not found");
+    }
+    res.status(200).json(user);
 
 });
 
 const deleteUser = async(req:Request,res:Response)=>{
-
+    const user = await deleteUserService(Number(req.body.params));
+        if(!user){
+        res.status(404)
+        throw new Error("user not found");
+    }
+    res.status(200).json(user);
 }
 
 const updateUser = async(req:Request,res:Response)=>{
+    const user = await updateUserService(
+        Number(req.body.params),
+        req.body.email,
+        req.body.fullName,
+        req.body.password
+    )
+    if(!user){
+        res.status(404)
+        throw new Error("user not found");
+    }
+    res.status(200).json(user);
 
 }
 module.exports = {createUser,getUser,deleteUser,updateUser};
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,16 +1,35 @@
-const createUserService = async()=>{
+import {pool} from "../config/db"
+
+export const createUserService = async(email:string,fullName:string,password:string)=>{
+
+    const result = await pool.query( `INSERT INTO tbluser (email, fullName, password)
+     VALUES ($1, $2, $3)
+     RETURNING *`,
+    [email, fullName, password]);
+    return result.rows[0];
+   
+}
+
+export const getUserService = async(id:number)=>{
+
+  const result = await pool.query(`SELECT * from tbluser where id = $1`,[id]);
+  return result.rows[0];
 
 }
 
-const getUserService = async()=>{
+export const deleteUserService = async(id:number)=>{
+    const result = await pool.query(`DELETE FROM tbluser WHERE id = $1 RETURNING *`,[id]);
+    return result.rows[0];
 
 }
 
-const deleteUserService = async()=>{
-
-}
-
-const updateUserService = async()=>{
+export const updateUserService = async(id:number,email:string,fullName:string,password:string)=>{
+      const result = await pool.query(`UPDATE tbluser
+     SET email = $1, fullName = $2, password = $3, updatedAt = NOW()
+     WHERE id = $4
+     RETURNING *`,
+    [email, fullName, password, id]);
+   return result.rows[0];
 
 }
 
